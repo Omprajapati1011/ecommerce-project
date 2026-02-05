478404b6e1d9756312cf7a978d25b6f61078e5db
Omprajapati1011 <om313989@gmail.com>
2026-02-05 11:01:24 +0530

    WIP: update server config

---
+++ BACKEND/configs/db.js
@@ -0,0 +1,29 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+ 
+pool.getConnection((err, connection)=>{
+    if(err){
+        console.log("connection failed done",err.message);
+    }else{
+        console.log("DB connection Done");
+        connection.release();
+    }
+})
+
+
+
+export default pool;
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,1 +1,18 @@
-//devang
+import express from "express";
+import dotenv from "dotenv";
+import pool from "./configs/db.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req, res) => {
+ res.send("Om prajapati");
+});
+
+app.listen(port, ()=>{
+      console.log(`Server is running on http://localhost:${port}`);
+});
